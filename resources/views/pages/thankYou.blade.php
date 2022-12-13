4aa118f1f88b1221c3028b1be41694b983cfd0ae
bhavdeepbhardwaj <bhavdeepbhardwaj555@gmail.com>
2022-12-13 18:10:17 +0530

    V1.1

--- resources/views/pages/thankYou.blade.php
+++ resources/views/pages/thankYou.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="GLOBALSYNC">
+    <meta name="description" content="Globalsync">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <!-- FAVICON -->
@@ -18,16 +18,7 @@
     <link rel="mask-icon" href="{{ asset('assets/img/icons/safari-pinned-tab.svg" color="#5bbad5 ') }}">
 
     <!-- Bootstrap CSS -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800&family=Poppins:wght@300;400;500;600;700;800;900&family=Roboto:wght@400;500;700;900&display=swap"
-        rel="stylesheet">
-
-    <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
-    <!-- globalsync CSS -->
-    <link id="globalsync-css" href="{{ asset('assets/css/globalsync.css ') }}" rel="stylesheet" />
     <link id="globalsync-css" href="{{ asset('assets/css/demo.css ') }}" rel="stylesheet" />
-
-
 
     <style>
         li {
@@ -42,7 +33,7 @@
         #results { padding:20px; border:1px solid; background:#ccc; }
     </style> --}}
 
-    <title>Globalsync</title>
+    <title>GLOBALSYNC THANK YOU</title>
 </head>
 
 <body>
@@ -52,32 +43,31 @@
                 <!-- CONTENT WRAPPER -->
                 <div class="container">
                     <div class="row justify-content-center">
-                        <div class="col-md-12 col-12 col-lg-12">
-                            <div class="card">
-                                <div class="card bg-white profile-content" style="padding: 22px!important;">
-                                    <h1>Thank You</h1>
-                                </div>
-                            </div>
+                        <div class=" text-center">
+                            <img class="pt-5" src="{{ asset('assets/img/logo/logo.png') }}" alt="Globalsync" />
+                        </div>
+
+                        <div class="justify-content-between align-items-center mb-3" style="margin-top: 100px!important">
+                            <h4>Dear Customer,</h4>
+                            <p>We are working on your dashboard as per your requirement.</p>
+                            <p>You will shortly receive all details from us and confirmation to login and access
+                                dashboard.</p>
                         </div>
                     </div>
                 </div>
                 <!-- End Content Wrapper -->
             </div>
         </div>
-
     </section>
 
     <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 
     <!-- globalsync Custom -->
-    <script src="{{ asset('assets/js/globalsync.js ') }}"></script>
-    <script src="{{ asset('assets/js/html2pdf.js ') }}"></script>
+    {{-- <script src="{{ asset('assets/js/globalsync.js ') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/js/html2pdf.js ') }}"></script> --}}
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <script>
+    {{-- <script>
         function generatePDF() {
             var doc_name = "Visitor.pdf";
             var element = document.getElementById('converttoPDF');
@@ -88,7 +78,7 @@
                 }
             }).from(element).save(doc_name);
         }
-    </script>
+    </script> --}}
 
 </body>
 
